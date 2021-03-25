ae78d102478b89ee2c62d7df4b074f06a34a58df
VladWithCode <vladwithcode@gmail.com>
2021-03-25 16:09:41 -0600

    Route protection enabled again

--- public/js/helpers/help.js
+++ public/js/helpers/help.js
@@ -4,6 +4,9 @@ export function handleFetchErrors(res) {
 }
 
 export function generateStateValueHTML(item) {
-  console.log(item)
-  return `<div class="state__value${item.state ? ' state__value--a' : ' state__value--u'}" data-availability="${item.state ? 'available' : 'unavailable'}">${item.name}</div>`
+  return `<div class="state__value${
+    item.state ? ' state__value--a' : ' state__value--u'
+  }" data-availability="${item.state ? 'available' : 'unavailable'}">${
+    item.name
+  }</div>`;
 }
